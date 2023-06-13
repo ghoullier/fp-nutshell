9f461ff556375cc793a2e9d1622658da20ac2d55
Grégory Houllier <gregory.houllier@gmail.com>
2023-06-13 23:06:01 +0200

    feat(typing): support const type parameters (#49)

--- src/Option.ts
+++ src/Option.ts
@@ -8,12 +8,12 @@ interface None<Value> {}
 
 type $Option<Value> = Some<Value> | None<Value>;
 
-export class Option<Value> {
+export class Option<const Value> {
   readonly #option: $Option<Value>;
-  static Some<Value>(value: Value): Option<Value> {
+  static Some<const Value>(value: Value): Option<Value> {
     return new Option({ value });
   }
-  static None<Value>(): Option<Value> {
+  static None<const Value>(): Option<Value> {
     return new Option({});
   }
   private constructor(option: $Option<Value>) {
@@ -34,13 +34,13 @@ export class Option<Value> {
     }
     return "None";
   }
-  map<Result>(mapper: Mapper<Value, Result>): Option<Result> {
+  map<const Result>(mapper: Mapper<Value, Result>): Option<Result> {
     if (!this.isOk()) {
       return Option.None();
     }
     return Option.Some(mapper((this.#option as Some<Value>).value));
   }
-  flatMap<Result>(mapper: Mapper<Value, Option<Result>>): Option<Result> {
+  flatMap<const Result>(mapper: Mapper<Value, Option<Result>>): Option<Result> {
     if (!this.isOk()) {
       return Option.None();
     }
