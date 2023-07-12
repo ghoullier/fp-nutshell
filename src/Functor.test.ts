34286a8b5ffc9efe11c5527fbaab3c077657aaba
Grégory Houllier <gregory.houllier@gmail.com>
2023-07-12 22:39:33 +0200

    feat: add describe test wrapper (#59)

--- src/Functor.test.ts
+++ src/Functor.test.ts
@@ -1,10 +1,12 @@
-import { expect, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 
 import { Functor } from "./Functor.js";
 
-test("should map value without change the original reference", () => {
-  const content = new Functor("content");
-  const html = content.map((value) => `<div>${value}</div>`);
-  expect(content.getValue()).toBe("content");
-  expect(html.getValue()).toBe("<div>content</div>");
+describe("Functor", () => {
+  test("should map value without change the original reference", () => {
+    const content = new Functor("content");
+    const html = content.map((value) => `<div>${value}</div>`);
+    expect(content.getValue()).toBe("content");
+    expect(html.getValue()).toBe("<div>content</div>");
+  });
 });
