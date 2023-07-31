0df1801ac84403bc6df2c362039ea734106f2d67
Grégory Houllier <gregory.houllier@gmail.com>
2023-07-31 16:10:42 +0000

    feat: Migrate Option and Result to Container

--- src/Result.ts
+++ src/Result.ts
@@ -1,3 +1,4 @@
+import { Container } from "./Container.js";
 import { Option } from "./Option.js";
 
 import type { Mapper } from "./Mapper.js";
@@ -12,53 +13,49 @@ interface Success<Value> {
 
 type $Result<Value, Error> = Success<Value> | Failure<Error>;
 
-export class Result<const Value, const Error> {
-  readonly #result: $Result<Value, Error>;
+export class Result<const Value, const Error> extends Container<$Result<Value, Error>>{
   static Ok<Value, Error>(value: Value): Result<Value, Error> {
     return new this({ value });
   }
   static Error<Value, Error>(error: Error): Result<Value, Error> {
     return new this({ error });
   }
-  private constructor(result: $Result<Value, Error>) {
-    this.#result = result;
-  }
   isError(): boolean {
-    return "error" in this.#result;
+    return "error" in this.value;
   }
-  toString(): string {
+  override toString(): string {
     if (this.isError()) {
-      return `Error ( ${String((this.#result as Failure<Error>).error)} )`;
+      return `Error ( ${String((this.value as Failure<Error>).error)} )`;
     }
-    return `Ok ( ${String((this.#result as Success<Value>).value)} )`;
+    return `Ok ( ${String((this.value as Success<Value>).value)} )`;
   }
   map<Output>(mapper: Mapper<Value, Output>): Result<Output, Error> {
     if (this.isError()) {
-      return Result.Error((this.#result as Failure<Error>).error);
+      return Result.Error((this.value as Failure<Error>).error);
     }
-    return Result.Ok(mapper((this.#result as Success<Value>).value));
+    return Result.Ok(mapper((this.value as Success<Value>).value));
   }
   flatMap<Output>(
     mapper: Mapper<Value, Result<Output, Error>>
   ): Result<Output, Error> {
     if (this.isError()) {
-      return Result.Error((this.#result as Failure<Error>).error);
+      return Result.Error((this.value as Failure<Error>).error);
     }
-    return mapper((this.#result as Success<Value>).value);
+    return mapper((this.value as Success<Value>).value);
   }
   match<Output>(
     resolve: Mapper<Value, Output>,
     reject: Mapper<Error, Output>
   ): Output {
     if (this.isError()) {
-      return reject((this.#result as Failure<Error>).error);
+      return reject((this.value as Failure<Error>).error);
     }
-    return resolve((this.#result as Success<Value>).value);
+    return resolve((this.value as Success<Value>).value);
   }
   ok(): Option<Value> {
     if (this.isError()) {
       return Option.None();
     }
-    return Option.Some((this.#result as Success<Value>).value);
+    return Option.Some((this.value as Success<Value>).value);
   }
 }
