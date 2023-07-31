f59bb2237908c1466308579c58cf776eb9d2d6f0
Grégory Houllier <gregory.houllier@gmail.com>
2023-07-31 18:17:36 +0200

    feat: Migrate Option and Result to Container (#83)

--- src/Option.ts
+++ src/Option.ts
@@ -1,3 +1,4 @@
+import { Container } from "./Container.js";
 import type { Mapper } from "./Mapper.js";
 
 interface Some<Value> {
@@ -8,29 +9,25 @@ interface None {}
 
 type $Option<Value> = Some<Value> | None;
 
-export class Option<const Value> {
-  readonly #option: $Option<Value>;
+export class Option<const Value> extends Container<$Option<Value>>{
   static Some<const Value>(value: Value): Option<Value> {
     return new Option({ value });
   }
   static None<const Value>(): Option<Value> {
     return new Option({});
   }
-  private constructor(option: $Option<Value>) {
-    this.#option = option;
-  }
   isOk(): boolean {
-    return "value" in this.#option;
+    return "value" in this.value;
   }
   orElse(fallback: Value): Value {
     if (this.isOk()) {
-      return (this.#option as Some<Value>).value;
+      return (this.value as Some<Value>).value;
     }
     return fallback;
   }
-  toString(): string {
+  override toString(): string {
     if (this.isOk()) {
-      return `Some(${String((this.#option as Some<Value>).value)})`;
+      return `Some(${String((this.value as Some<Value>).value)})`;
     }
     return "None";
   }
@@ -38,12 +35,12 @@ export class Option<const Value> {
     if (!this.isOk()) {
       return Option.None();
     }
-    return Option.Some(mapper((this.#option as Some<Value>).value));
+    return Option.Some(mapper((this.value as Some<Value>).value));
   }
   flatMap<const Result>(mapper: Mapper<Value, Option<Result>>): Option<Result> {
     if (!this.isOk()) {
       return Option.None();
     }
-    return mapper((this.#option as Some<Value>).value);
+    return mapper((this.value as Some<Value>).value);
   }
 }
