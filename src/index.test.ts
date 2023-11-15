56508045707a0b28a297da43a33aa5628968778e
Grégory Houllier <gregory.houllier@gmail.com>
2023-11-15 06:14:20 +0000

    chore: test top level exports

---
+++ src/index.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from "bun:test";
+
+import * as fp from "./index.js"
+
+describe("@ghoullier/fp-nutshell", () => {
+    test("should exports expected classes", ()=> {
+        expect(fp.Container).toBeDefined()
+        expect(fp.Functor).toBeDefined()
+        expect(fp.Monad).toBeDefined()
+        expect(fp.Option).toBeDefined()
+        expect(fp.Result).toBeDefined()
+    })
+})
