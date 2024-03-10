b3f4cd3ffe1076949f5d104bd883e4d06fafedfb
Grégory Houllier <gregory.houllier@gmail.com>
2024-03-10 08:05:55 +0000

    chore: update jsr implementation

--- scripts/prerelease.ts
+++ scripts/prerelease.ts
@@ -1,20 +1,21 @@
 import { EOL } from "node:os";
 
 interface Manifest {
+  name: string
   version: string;
 }
 
 const jsr = Bun.file("./jsr.json", { type: "application/json" });
 const pkg = Bun.file("./package.json", { type: "application/json" });
 const pkgContent: Manifest = await pkg.json();
-const jsrContent: Manifest = await jsr.json();
 
 await Bun.write(
   jsr,
   `${JSON.stringify(
     {
-      ...jsrContent,
+      name: pkgContent.name,
       version: pkgContent.version,
+      exports: "./src/index.ts"
     },
     null,
     2
